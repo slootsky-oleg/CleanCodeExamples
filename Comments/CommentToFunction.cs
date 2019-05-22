0b05068c907e622777481173a4d1c0bdc180687e
Oleg Slutsky <slootsky@gmail.com>
2019-05-22 11:04:02 +0300

    Add comments examples

---
+++ Comments/CommentToFunction.cs
@@ -0,0 +1,30 @@
+﻿namespace Comments.CommentToVariable
+{
+	public class CommentToFunction
+	{
+		public void Linq()
+		{
+			BEFORE:
+			//get candidates whose training position was chaged
+			var toUpdateTrainingPos = (from toReg in candidates
+				join exists in participants on toReg.ID equals exists.ID
+				where toReg.EntityType == Entity.EntitiesTypes.User && toReg.TrainingPositionID != exists.PositionID
+				select toReg).ToList();
+
+			AFTER:
+			var changedTrainingPositions = GetChangedTrainingPositions(candidates, participants,...);
+
+
+
+			BEFORE:
+			var filteredEvents = events
+				.AsEnumerable()
+				.Where(dr => !eventIDs.Contains(dr.GetNumericProperty("CalID")) && !eventIDs.Contains(dr.GetNumericProperty("CollectionID")));
+
+			AFTER:
+			var filteredEvents = events
+				.AsEnumerable()
+				.Where(dr => !EventAlreadyProcessed(eventIDs, dr));
+		}
+	}
+}
---
+++ Comments/CommentToVariable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Comments.CommentToVariable
+{
+	public class CommentToVariable
+	{
+		public void CommentToVariable()
+		{
+			FoxAdministration.Course currCourse = FoxAdministration.AdministrationEnityFactory.CreateCourse<FoxAdministration.Course>(CurrPortal.OriginalEntityID);
+
+			BEFORE:
+			/* Also violates DRY - Don't repeat yourself*/
+			//If tp dos not have end date (01/01/2100) than hide the end date field
+			if (currCourse.EndDate == DateTimeCommon.MaxFutureDate)
+				EndDateTr.Visible = false;
+
+			AFTER:
+			if (currCourse.HasEndDate)
+				EndDateTr.Visible = false;
+
+
+
+			BEFORE:
+			if (SystemConfiguration.EnableReferToLatestMasterPlanVersion && currCourse.ID > 0 && currCourse.CurrentCourseTemplateID > 0)
+				trReferredToMasterPlanName.Style.Remove("display");
+			...
+			if (SystemConfiguration.EnableReferToLatestMasterPlanVersion && currCourse.ID > 0 && currCourse.CurrentCourseTemplateID > 0)
+				MasterPlanNameReferredToLink.Visible = true;
+
+			AFTER:
+			var referToLatestMasterPlanVersion = 
+				SystemConfiguration.EnableReferToLatestMasterPlanVersion 
+				&& currCourse.ID > 0 
+				&& currCourse.CurrentCourseTemplateID > 0;
+
+			if (referToLatestMasterPlanVersion)
+				trReferredToMasterPlanName.Style.Remove("display");
+					...
+			if (referToLatestMasterPlanVersion)
+				MasterPlanNameReferredToLink.Visible = true;
+
+
+
+			BEFORE:
+			var mp = currCourse.CourseTemplate;
+				...
+			if (IsUserManager 
+			    && currCourse.WasCreatedBasedOnFutureBaselineVersion
+			    && mp.EffectiveDate > DateTime.Today 
+			    && mp.VersionStatus == BOL.Enums.VersionStatus.FutureBaseline)
+
+			AFTER:
+			if (IsUserManager && currCourse.IsActiveFutureBaseLine)
+		}
+	}
+}
