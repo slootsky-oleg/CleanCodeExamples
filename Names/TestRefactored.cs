80b42bc1dcc1e355579f96de1c06b25a4a3b98e4
Oleg Slutsky <slootsky@gmail.com>
2019-05-22 11:32:46 +0300

    Test answers for names project

---
+++ Names/TestRefactored.cs
@@ -0,0 +1,63 @@
+﻿namespace Names
+{
+	public class ActivityManager
+	{
+		private int l = 50;
+
+		public string actNameInTP; //Activity name in the training program
+		public string actNameInST; //Activity name in the source template
+		public string actNameInBL; //Activity name in the base line
+
+		public string GetActualActNameByItsNameSource()
+		{
+			if (!string.IsNullOrWhiteSpace(actNameInTP))
+			{
+				return actNameInTP;
+			}
+			else if (!string.IsNullOrWhiteSpace(actNameInST))
+			{
+				return actNameInST;
+			}
+			else if (!string.IsNullOrWhiteSpace(actNameInBL))
+			{
+				return actNameInBL;
+			}
+
+			return string.Empty;
+		}
+
+		public string GetActualActNameByItsNameSourceLeftSymbols()
+		{
+			var n = string.Empty;
+
+			if (!string.IsNullOrWhiteSpace(actNameInTP))
+			{
+				n = actNameInTP.ToLower();
+			}
+			else if (!string.IsNullOrWhiteSpace(actNameInST))
+			{
+				n = actNameInST.ToLower();
+			}
+			else if (!string.IsNullOrWhiteSpace(actNameInBL))
+			{
+				n =actNameInBL.ToLower();
+			}
+
+			return n.Substring(0, l);
+		}
+	}
+
+	public class ActivityManagerDemo
+	{
+		public void Demo()
+		{
+			var manager = new ActivityManager
+			{
+				actNameInST = "name in Source template"
+			};
+
+			var name = manager.GetActualActNameByItsNameSource();
+			var shortName = manager.GetActualActNameByItsNameSourceLeftSymbols();
+		}
+	}
+}
